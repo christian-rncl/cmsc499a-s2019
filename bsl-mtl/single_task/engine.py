53a18be91cd605cf6e5f9aef1b668601f3707584
Christian Roncal <cjl.roncal@gmail.com>
2019-03-01 11:38:10 -0500

    finished single batch code

--- bsl-mtl/single_task/engine.py
+++ bsl-mtl/single_task/engine.py
@@ -12,8 +12,28 @@ class Engine(object):
     
     def __init__(self, config):
         self.config = config
+        self.interactions = config['interactions']
         self.opt = use_optimizer(self.model, config)
         self.crit = torch.nn.MSELoss()
 
     def train_a_batch(self, x, y, idxs):
         # make sure have model attr with assert
+        # tensor of 1's for comparison
+        gt = torch.from_numpy(np.ones(len(idxs)))
+
+        if self.config['cuda'] = True:
+            x, y, idx = x.cuda(), y.cuda(), idxs.cuda()
+        
+        self.opt.zero_grad()
+        pred = self.model(user)
+        loss = self.crit(pred, gt)
+        loss.backward()
+        self.opt.step()
+        
+        if self.config['use_cuda'] is True:
+            loss = loss.data.cpu().numpy()[0]
+        else:
+            loss = loss.data.numpy()[0]
+        return loss
+
+        
