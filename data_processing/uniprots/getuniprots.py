3f7b654a68857538cbfb093e83dc7508a69888de
Christian Roncal <cjl.roncal@gmail.com>
2019-04-03 01:51:07 -0400

    finished feats but encountered bio error for human proteins

--- data_processing/uniprots/getuniprots.py
+++ data_processing/uniprots/getuniprots.py
@@ -1,17 +1,20 @@
-import urllib,urllib2
+import requests
 
 url = 'https://www.uniprot.org/uploadlists/'
 
-params = {
+payload = {
 'from':'ACC',
 'to':'P_REFSEQ_AC',
 'format':'tab',
 'query':'P13368 P20806 Q9UM73 P97793 Q17192'
 }
 
-data = urllib.urlencode(params)
-request = urllib2.Request(url, data)
-contact = "" # Please set a contact email address here to help us debug in case of problems (see https://www.uniprot.org/help/privacy).
-request.add_header('User-Agent', 'Python %s' % contact)
-response = urllib2.urlopen(request)
-page = response.read(200000)
+r = requests.get(url, payload)
+print(r.text)
+
+# data = urllib.urlencode(params)
+# request = urllib2.Request(url, data)
+# contact = "" # Please set a contact email address here to help us debug in case of problems (see https://www.uniprot.org/help/privacy).
+# request.add_header('User-Agent', 'Python %s' % contact)
+# response = urllib2.urlopen(request)
+# page = response.read(200000)
