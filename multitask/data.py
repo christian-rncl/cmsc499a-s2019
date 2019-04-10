6cb2bafe3f3ffd63f904c3ba074ccfb91a914ffa
Christian Roncal <cjl.roncal@gmail.com>
2019-04-10 13:56:47 -0400

    fixed dataloader for new&improved data

---
+++ multitask/data.py
@@ -0,0 +1,86 @@
+'''
+Author(s): Christian Roncal
+Leiserson Research Group 2/28/2019
+'''
+import torch
+import pandas as pd
+import numpy as np
+import numpy.random as rand
+from torch.utils.data import DataLoader, Dataset, TensorDataset
+from sklearn.model_selection import train_test_split
+
+'''
+    Single task experiments data generator,
+    Given params interaction matrix, human/virus features for a single task, 
+    process data and split to train/cv/test based on settings 
+'''
+class ProteinInteractionGenerator(object):
+    '''
+    interactions: df representing interactions
+    human: np array 
+    '''
+    # def __init__(self, interactions, human_feats, virus_feats, htoi, vtoi, pct_test, device):
+    def __init__(self, config):
+        self.htoi = config['htoi']
+        self.vtoi = config['vtoi']
+        # index interactions based on htoi and vtoi
+        self.index_interactions(config['interactions'])
+        self.hfeats = config['hfeats']
+        self.vfeats = config['vfeats']
+        # # self.device = torch.device('cuda') if cuda else torch.device('cpu')
+        self.device = config['device']
+        print('using device: ', self.device)
+
+        self.split_data(config['pct_test'])
+
+    def split_data(self, pct_test):
+        # X = self.interactions.drop(['virusUprot', 'humanUprot'], axis=1).values
+        X = self.interactions[['virusUprot', 'humanUprot']]
+        y = self.interactions['edge']
+        n_pos = len(y[y > 0])
+        n_neg = len(y) - n_pos
+        print(f'Found {n_pos} positives, and {n_neg} negatives!', n_pos/len(y))
+
+        self.Xtrain, self.Xtest, self.yTrain, self.yTest = train_test_split(X,y, test_size=pct_test, random_state=42)
+        self.Xtrain, self.Xval, self.yTrain, self.yVal = train_test_split(self.Xtrain, self.yTrain, test_size=.10, random_state=42)
+
+        train_pos = len(self.yTrain[self.yTrain > 0])
+        val_pos = len(self.yVal[self.yVal > 0])
+        test_pos = len(self.yTest[self.yTest > 0])
+
+        print(8 * '-')
+        print(f'{train_pos} in training set, {train_pos/len(self.yTrain)}')
+        print(f'{val_pos} in val set, {train_pos/len(self.yVal)}')
+        print(f'{test_pos} in test set, {test_pos/len(self.yTest)}')
+    
+    def index_interactions(self, interactions):
+        self.interactions = interactions
+        self.interactions['virusUprot'] = self.interactions['virusUprot'].apply(lambda x: self.vtoi[x])
+        self.interactions['humanUprot'] = self.interactions['humanUprot'].apply(lambda x: self.htoi[x])
+
+
+    def create_train_loader(self, bs):
+        return self.create_loader(self.Xtrain, self.yTrain, bs)
+
+    def create_val_loader(self, bs):
+        return self.create_loader(self.Xval, self.yVal, bs)
+
+    def create_test_loader(self, bs):
+        return self.create_loader(self.Xtest, self.yTest, bs)
+
+    def create_debug_loader(self, bs):
+        return self.create_loader(self.Xtest[:10], self.yVal[:10], bs)
+        
+    def create_loader(self, dsetX, dsetY, bs):
+        virus_idxs = dsetX['virusUprot'].values
+        human_idxs = dsetX['humanUprot'].values
+        ys = dsetY.values
+
+        dset = TensorDataset(
+            torch.from_numpy(virus_idxs).to(self.device),
+            torch.from_numpy(human_idxs).to(self.device),
+            torch.from_numpy(ys).to(self.device))
+        
+        return DataLoader(dset, batch_size=bs, shuffle=True)
+
+
---
+++ multitask/gmf.py
@@ -0,0 +1,70 @@
+'''
+Author(s): Christian Roncal
+Leiserson Research Group March 1
+based on: https://github.com/LaceyChen17/neural-collaborative-filtering/blob/master/src/gmf.py
+'''
+
+import torch
+import torch.nn as nn
+# from engine import Engine
+from utils import use_cuda
+
+class GMF(nn.Module):
+
+    def create_embeddings(self, n_embeddings, dim, sparsity):
+        if(self.sparse):
+            embedding = nn.Embedding(
+                num_embeddings = n_embeddings, 
+                embedding_dim = dim, 
+                sparse = self.sparse)
+
+            nn.init.sparse_(embedding.weight, sparsity=self.sparsity)
+
+            return embedding
+
+        else: 
+            embedding = nn.Embedding(
+                num_embeddings = n_embeddings, 
+                embedding_dim = dim)
+            nn.init.xavier_normal_(embedding.weight)
+
+            return embedding
+
+    def __init__(self, config):
+        super(GMF, self).__init__()
+
+        self.num_virus = config['num_virus']
+        self.num_human = config['num_human']
+        self.latent_dim = config['latent_dim']
+        self.sparse = config['sparse']
+
+        # % of elements / col to be set to 0
+        if(self.sparse):
+            self.sparsity = config['sparsity']
+
+        self.virus, self.human = [self.create_embeddings(*dims, self.sparse) 
+        for dims in [(self.num_virus, self.latent_dim), (self.num_human, self.latent_dim)]
+        ]
+        ## TODO: experiment with bias
+
+    def forward(self, h_idxs, v_idxs):
+
+        assert(len(h_idxs) == len(v_idxs))
+        U_xi = self.human(h_idxs)
+        V_yj = self.virus(v_idxs)
+        # except:
+        # xUVy = (U_xi.double() * h_feats.double()).sum(1)  # xU
+        # xUVy = (xUVy.double() * V_yj.t().double()).sum(1) # xUV
+        # xUVy = (xUVy.double() * v_feats.double()).sum(1) # xUVy
+
+        # print(xUVy)
+        return xUVy
+
+# class GMFEngine(Engine):
+#     def __init__(self, config):
+#         self.model = GMF(config)
+#         if config['cuda']:
+#             use_cuda(True)
+#             self.model.cuda()
+
+#         super(GMFEngine, self).__init__(config)
